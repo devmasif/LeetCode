e1b447f4cfd58d5291ba57e766e5b604b837915c
devmasif <m.ali83075@gmail.com>
2025-07-07 20:18:47 +0500

    update

--- Day11-ValidParenthesis/solution.cpp
+++ Day11-ValidParenthesis/solution.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-	bool isValid(string s) {
-		stack<char> stk;
+    bool isValid(string s) {
+        stack<char> stk;
+        int idx = 0;
 
-		int idx = 0;
-		while(s[idx] != '\0') {
-			if(s[idx] == '(' || s[idx] == '{' || s[idx] == '[')
-				stk.push(s[idx]);
-			else {
-				if (stk.empty()) return false;
+        while (s[idx] != '\0') {
+            if (s[idx] == '(' || s[idx] == '{' || s[idx] == '[') {
+                stk.push(s[idx]);
+            } else {
+                if (stk.empty()) return false;
 
-				        if( (s[idx] == ')' && stk.top() == '(')
-					            ||    (s[idx] == '}' && stk.top() == '{')
-					            ||    (s[idx] == ']' && stk.top() == '[')  )
-					        stk.pop();
-					        else
-						        return false;
-
-						        
-					
+                if ((s[idx] == ')' && stk.top() == '(') ||
+                    (s[idx] == '}' && stk.top() == '{') ||
+                    (s[idx] == ']' && stk.top() == '[')) {
+                    stk.pop();
+                } else {
+                    return false;
+                }
             }
             idx++;
         }
 
-		if(stk.empty())
-				return true;
-
-			return false;
-		
-	}
-
+        return stk.empty();
+    }
 };
-
